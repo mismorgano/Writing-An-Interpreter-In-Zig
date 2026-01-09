f5c01f066e265d252c899a52f0643dfcd98164b9
mismorgano <mismorgano@gmail.com>
2026-01-08 19:38:33 -0600

    update: lexer_test to include a more robust and complete example of monkey code

--- monkey/src/lexer/lexer_test.zig
+++ monkey/src/lexer/lexer_test.zig
@@ -7,18 +7,55 @@ const std = @import("std");
 const expect = @import("std").testing.expect;
 
 test "next token" {
-    const input = "=+(){},;";
+    const input =
+        \\let five = 5;
+        \\let ten = 10;
+        \\
+        \\let add = fn(x, y) {
+        \\ x + y;
+        \\}; 
+        \\
+        \\let result = add(five, ten);
+    ;
 
     const ExpectedToken = struct { expected_type: token.TokenType, expected_literal: []const u8 };
 
     const tests = [_]ExpectedToken{
+        .{ .expected_type = token.LET, .expected_literal = "let" },
+        .{ .expected_type = token.IDENT, .expected_literal = "five" },
         .{ .expected_type = token.ASSIGN, .expected_literal = "=" },
-        .{ .expected_type = token.PLUS, .expected_literal = "+" },
+        .{ .expected_type = token.INT, .expected_literal = "5" },
+        .{ .expected_type = token.SEMICOLON, .expected_literal = ";" },
+        .{ .expected_type = token.LET, .expected_literal = "let" },
+        .{ .expected_type = token.IDENT, .expected_literal = "ten" },
+        .{ .expected_type = token.ASSIGN, .expected_literal = "=" },
+        .{ .expected_type = token.INT, .expected_literal = "10" },
+        .{ .expected_type = token.SEMICOLON, .expected_literal = ";" },
+        .{ .expected_type = token.LET, .expected_literal = "let" },
+        .{ .expected_type = token.IDENT, .expected_literal = "add" },
+        .{ .expected_type = token.ASSIGN, .expected_literal = "=" },
+        .{ .expected_type = token.FUNCTION, .expected_literal = "fn" },
         .{ .expected_type = token.LPAREN, .expected_literal = "(" },
+        .{ .expected_type = token.IDENT, .expected_literal = "x" },
+        .{ .expected_type = token.COMMA, .expected_literal = "," },
+        .{ .expected_type = token.IDENT, .expected_literal = "y" },
         .{ .expected_type = token.RPAREN, .expected_literal = ")" },
         .{ .expected_type = token.LBRACE, .expected_literal = "{" },
+        .{ .expected_type = token.IDENT, .expected_literal = "x" },
+        .{ .expected_type = token.PLUS, .expected_literal = "+" },
+        .{ .expected_type = token.IDENT, .expected_literal = "y" },
+        .{ .expected_type = token.SEMICOLON, .expected_literal = ";" },
         .{ .expected_type = token.RBRACE, .expected_literal = "}" },
+        .{ .expected_type = token.SEMICOLON, .expected_literal = ";" },
+        .{ .expected_type = token.LET, .expected_literal = "let" },
+        .{ .expected_type = token.IDENT, .expected_literal = "result" },
+        .{ .expected_type = token.ASSIGN, .expected_literal = "=" },
+        .{ .expected_type = token.IDENT, .expected_literal = "add" },
+        .{ .expected_type = token.LPAREN, .expected_literal = "(" },
+        .{ .expected_type = token.IDENT, .expected_literal = "five" },
         .{ .expected_type = token.COMMA, .expected_literal = "," },
+        .{ .expected_type = token.IDENT, .expected_literal = "ten" },
+        .{ .expected_type = token.RPAREN, .expected_literal = ")" },
         .{ .expected_type = token.SEMICOLON, .expected_literal = ";" },
         .{ .expected_type = token.EOF, .expected_literal = "" },
     };
